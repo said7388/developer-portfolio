c4ca67948f1bae3ec23c63b3b2336e728c8f0638
said7388 <mdabusaidriyaz@gmail.com>
2022-11-15 10:04:14 +0600

    add docker

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,4 +1,4 @@
-export { default as BlogPage } from './blog/index'
+export { default as BlogPage } from './Blog/index'
 export { default as HomePage } from './home/index'
-export { default as ProjectPage } from './project/index'
+export { default as ProjectPage } from './Project/index'
 

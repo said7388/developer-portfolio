1d0a15b3b393e51c598c72b8eafc3eab4dc81a0e
said7388 <mdabusaidriyaz@gmail.com>
2023-12-22 10:02:57 +0600

    update

--- app/components/homepage/experience.jsx
+++ app/components/homepage/experience.jsx
@@ -7,7 +7,8 @@ import experience from '/public/lottie/development.json';
 function Experience() {
   return (
     <div className="relative z-50 border-t my-12 lg:my-24 border-[#25213b]">
-      <div class="w-[100px] h-[100px] bg-violet-100 rounded-full absolute -top-0 left-[44%] translate-x-1/2 filter blur-3xl  opacity-20"></div>
+      
+      <div class="w-[100px] h-[100px] bg-violet-100 rounded-full absolute top-6 left-[42%] translate-x-1/2 filter blur-3xl  opacity-20"></div>
 
       <div class="flex justify-center -translate-y-[1px]">
         <div class="w-3/4">
@@ -34,7 +35,8 @@ function Experience() {
 
           <div>
             <div class="flex flex-col gap-6">
-              <div class=" border border-[#1f223c] bg-[#131835] text-[#c4c4e8] rounded-md w-full">
+              <div class=" border border-[#1f223c] transition-all duration-300 hover:border-pink-500 relative bg-[radial-gradient(145.05%_100%_at_55%_0%,#221c4a_0%,#0B1627_60.38%,#142133_78.16%)] text-gray-200 rounded-md w-full">
+                <div className=" bg-[url('/card.png')] absolute bottom-0 left-0 w-full h-full -z-10 bg-top bg-no-repeat"></div>
                 <div class="flex justify-center -translate-y-[1px]">
                   <div class="w-3/4">
                     <div class="h-[1px] bg-gradient-to-r from-transparent via-pink-500 to-transparent w-full">
@@ -57,20 +59,20 @@ function Experience() {
                 </div>
               </div>
 
-              <div class=" border border-[#1f223c] bg-[#131835] text-[#c4c4e8] rounded-md w-full">
+              <div class=" border border-[#1f223c] transition-all duration-300 hover:border-[#16f2b3] bg-[radial-gradient(145.05%_100%_at_55%_0%,#221c4a_0%,#0B1627_60.38%,#142133_78.16%)] text-gray-200 rounded-md w-full">
                 <div class="flex justify-center -translate-y-[1px]">
                   <div class="w-3/4">
-                    <div class="h-[1px] bg-gradient-to-r from-transparent via-green-600 to-transparent w-full">
+                    <div class="h-[1px] bg-gradient-to-r from-transparent via-[#16f2b3] to-transparent w-full">
                     </div>
                   </div>
                 </div>
 
                 <div className="p-3">
                   <div className="flex justify-center">
-                    <p className="text-sm text-green-600">(Jun 2021 - January 2022)</p>
+                    <p className="text-sm text-[#16f2b3]">(Jun 2021 - January 2022)</p>
                   </div>
                   <div class="flex items-center gap-x-8 p-4">
-                    <div className="text-green-600  transition-all duration-300 hover:scale-125">
+                    <div className="text-[#16f2b3]  transition-all duration-300 hover:scale-125">
                       <BsPersonWorkspace size={36} />
                     </div>
                     <div className="">
@@ -81,7 +83,7 @@ function Experience() {
                 </div>
               </div>
 
-              <div class=" border border-[#1f223c] bg-[#131835] text-[#c4c4e8] rounded-md w-full">
+              <div class=" border border-[#1f223c] transition-all duration-300 hover:border-violet-500 bg-[radial-gradient(145.05%_100%_at_55%_0%,#221c4a_0%,#0B1627_60.38%,#142133_78.16%)] text-gray-200 rounded-md w-full">
                 <div class="flex justify-center -translate-y-[1px]">
                   <div class="w-3/4">
                     <div class="h-[1px] bg-gradient-to-r from-transparent via-violet-500 to-transparent w-full">

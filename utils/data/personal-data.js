8db87e5657a454107ed4b185ad52b00b3cced448
Saleh Mohamed <61949488+0xManticore@users.noreply.github.com>
2024-03-18 07:30:47 +0800

    Update personal-data.js

--- utils/data/personal-data.js
+++ utils/data/personal-data.js
@@ -2,7 +2,7 @@ export const personalData = {
   name: "Saleh Elsayed",
   profile: '/profile2.jpg',
   designation: "CyberSecurity Consultant",
-  description: "Saleh Elsayed (AKA @0xmanticore); an offensive cybersecurity enthusiast Since 2018. With experience as a Security Consultant, Penetration Tester, Red Teamer, Bug Bounty Hunter, and a CTF player.",
+  description: "Saleh Elsayed (AKA @0xmanticore); an offensive cybersecurity enthusiast Since 2018. With experience as a Security Consultant, Penetration Tester, Red Teamer, Bug Bounty Hunter, and a CTF player.\n\nDeeply engaged in the Cyber Security community, participating and contributing actively and consistently during conferences, events, competitions and workshops in Malaysia, Egypt, Saudi Arabia, etc.",
   email: 'contact@0xmanticore.com',
   phone: '+601170073907',
   address: 'SS15, Subang Jaya, Selangor, Malaysia 47500',

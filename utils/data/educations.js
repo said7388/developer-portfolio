d988279d8036cc0f937aa9caff5bcf9c77dc361f
Saleh Mohamed <61949488+0xManticore@users.noreply.github.com>
2024-03-18 07:21:22 +0800

    Update educations.js

--- utils/data/educations.js
+++ utils/data/educations.js
@@ -1,20 +1,14 @@
 export const educations = [
   {
     id: 1,
-    title: "Bachelor Degree",
-    duration: "2020 - Present",
-    institution: "National University of Bangladesh",
+    title: "B.Sc. in Computer Science",
+    duration: "",
+    institution: "UTM (University of Technology Malaysia)",
   },
   {
     id: 2,
-    title: "Higher Secondary Certificate",
-    duration: "2018 - 2020",
-    institution: "Noakhali Islamia Kamil Madrasah",
-  },
-  {
-    id: 3,
-    title: "Secondary School Certificate",
-    duration: "2008 - 2018",
-    institution: "Baitus Saif Islamia Madrasah",
+    title: "PhD in Computer Science (Ongoing)",
+    duration: "",
+    institution: "UTM (University of Technology Malaysia)",
   }
 ]

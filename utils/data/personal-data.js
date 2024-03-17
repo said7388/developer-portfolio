2b521e6e5a0a6d3d00f67502f3b92515ef5c4d5f
Saleh Mohamed <61949488+0xManticore@users.noreply.github.com>
2024-03-18 07:17:02 +0800

    Update personal-data.js

--- utils/data/personal-data.js
+++ utils/data/personal-data.js
@@ -1,17 +1,17 @@
 export const personalData = {
   name: "Saleh Elsayed",
-  profile: '/profile.png',
+  profile: '/profile2.jpg',
   designation: "CyberSecurity Consultant",
-  description: "My name is ABU SAID. I am a professional and enthusiastic programmer in my daily life. I am a quick learner with a self-learning attitude. I love to learn and explore new technologies and am passionate about problem-solving. I love almost all the stacks of web application development and love to make the web more open to the world. My core skill is based on JavaScript and I love to do most of the things using JavaScript. I am available for any kind of job opportunity that suits my skills and interests.",
+  description: "Saleh Elsayed (AKA @0xmanticore); an offensive cybersecurity enthusiast Since 2018. With experience as a Security Consultant, Penetration Tester, Red Teamer, Bug Bounty Hunter, and a CTF player.",
   email: 'contact@0xmanticore.com',
   phone: '+601170073907',
   address: 'SS15, Subang Jaya, Selangor, Malaysia 47500',
   github: 'https://github.com/0xManticore',
-  facebook: 'https://www.facebook.com/abusaid.riyaz/',
+  facebook: 'https://www.facebook.com/',
   linkedIn: 'https://www.linkedin.com/in/0xManticore/',
   twitter: 'https://twitter.com/0xManticore',
-  stackOverflow: 'https://stackoverflow.com/users/16840768/abu-said',
-  leetcode: "https://leetcode.com/said3812/",
+  stackOverflow: 'https://stackoverflow.com/',
+  leetcode: "https://leetcode.com/",
   devUsername: "0xManticore",
-  resume: "https://drive.google.com/file/d/1eyutpKFFhJ9X-qpQGKhUNnVRkB5Wer00/view?usp=sharing"
+  resume: "https://drive.google.com/file/d/1hygmsMOb1flxrp3cnQDisGzrWUJiGOQm/view?usp=sharing"
 }

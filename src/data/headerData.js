7e8cd046363427ceee872724c1bff82892abbbd5
Nagarjun H bharadwaj <119668048+Nagarjun-07@users.noreply.github.com>
2024-04-13 13:14:02 +0000

    portfolio

--- src/data/headerData.js
+++ src/data/headerData.js
@@ -4,5 +4,5 @@ export const headerData = {
     desciption: " A coding enthusiast and an engineer. I am highly motivated and qualified individual with a strong passion for technology and problem-solving.  Love almost all the stacks of Software Engineering. My current larning stack includes Nodejs, Nestjs, React, Bootstrap, MongoDB, Mysql, Express Js etc. I welcome the opportunity to connect with professionals and other students in the engineering community to learn and grow.",
     image: 'https://avatars.githubusercontent.com/u/119668048?s=400&u=1c65d3ecbef73fea0a92aba5c298e93cd50e8edc&v=4',
     imagebw: 'https://drive.google.com/uc?export=view&id=1sTF2qB6BMvZFHAG7Wvv9X9YgbXxOuUts',
-    resumePdf: 'https://drive.google.com/file/d/1bRHJ33ZAXNR7dwPVhuDNsDE0hr-m-Xz_/view?usp=sharing'
+    resumePdf: 'https://drive.google.com/file/d/1walalEWb153CUj1Zk2kyTJMUL8WjG6F1/view?usp=drive_link'
 }

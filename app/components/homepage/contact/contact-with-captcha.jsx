89d2332b70a1e5bae9ecbd66ed7b44523c279b25
Nanahawaw <folarinyetunde@gmail.com>
2024-05-16 00:42:56 +0100

    run build

--- app/components/homepage/contact/contact-with-captcha.jsx
+++ app/components/homepage/contact/contact-with-captcha.jsx
@@ -1,11 +1,11 @@
-"use client";
+'use client';
 // @flow strict
 import { isValidEmail } from '@/utils/check-email';
 import emailjs from '@emailjs/browser';
 import axios from 'axios';
 import { useState } from 'react';
 import ReCAPTCHA from 'react-google-recaptcha';
-import { TbMailForward } from "react-icons/tb";
+import { TbMailForward } from 'react-icons/tb';
 import { toast } from 'react-toastify';
 
 function ContactWithCaptcha() {
@@ -31,16 +31,19 @@ function ContactWithCaptcha() {
       toast.error('Please complete the captcha!');
       return;
     } else {
-      const res = await axios.post(`${process.env.NEXT_PUBLIC_APP_URL}/api/google`, {
-        token: captcha
-      });
+      const res = await axios.post(
+        `${process.env.NEXT_PUBLIC_APP_URL}/api/google`,
+        {
+          token: captcha,
+        }
+      );
 
       setCaptcha(null);
       if (!res.data.success) {
         toast.error('Captcha verification failed!');
         return;
-      };
-    };
+      }
+    }
 
     e.preventDefault();
     if (!input.email || !input.message || !input.name) {
@@ -50,7 +53,7 @@ function ContactWithCaptcha() {
       return;
     } else {
       setError({ ...error, required: false });
-    };
+    }
 
     const serviceID = process.env.NEXT_PUBLIC_EMAILJS_SERVICE_ID;
     const templateID = process.env.NEXT_PUBLIC_EMAILJS_TEMPLATE_ID;
@@ -66,28 +69,30 @@ function ContactWithCaptcha() {
           email: '',
           message: '',
         });
-      };
+      }
     } catch (error) {
       toast.error(error?.text || error);
-    };
+    }
   };
 
   return (
-    <div className="">
-      <p className="font-medium mb-5 text-[#16f2b3] text-xl uppercase">
+    <div className=''>
+      <p className='font-medium mb-5 text-[#16f2b3] text-xl uppercase'>
         Contact with me
       </p>
-      <div className="max-w-3xl text-white rounded-lg border border-[#464c6a] p-3 lg:p-5">
-        <p className="text-sm text-[#d3d8e8]">
-          {"If you have any questions or concerns, please don't hesitate to contact me. I am open to any work opportunities that align with my skills and interests."}
+      <div className='max-w-3xl text-white rounded-lg border border-[#464c6a] p-3 lg:p-5'>
+        <p className='text-sm text-[#d3d8e8]'>
+          {
+            "If you have any questions or concerns, please don't hesitate to contact me. I am open to any work opportunities that align with my skills and interests."
+          }
         </p>
-        <div className="mt-6 flex flex-col gap-4">
-          <div className="flex flex-col gap-2">
-            <label className="text-base">Your Name: </label>
+        <div className='mt-6 flex flex-col gap-4'>
+          <div className='flex flex-col gap-2'>
+            <label className='text-base'>Your Name: </label>
             <input
-              className="bg-[#10172d] w-full border rounded-md border-[#353a52] focus:border-[#16f2b3] ring-0 outline-0 transition-all duration-300 px-3 py-2"
-              type="text"
-              maxLength="100"
+              className='bg-[#10172d] w-full border rounded-md border-[#353a52] focus:border-[#16f2b3] ring-0 outline-0 transition-all duration-300 px-3 py-2'
+              type='text'
+              maxLength='100'
               required={true}
               onChange={(e) => setInput({ ...input, name: e.target.value })}
               onBlur={checkRequired}
@@ -95,12 +100,12 @@ function ContactWithCaptcha() {
             />
           </div>
 
-          <div className="flex flex-col gap-2">
-            <label className="text-base">Your Email: </label>
+          <div className='flex flex-col gap-2'>
+            <label className='text-base'>Your Email: </label>
             <input
-              className="bg-[#10172d] w-full border rounded-md border-[#353a52] focus:border-[#16f2b3] ring-0 outline-0 transition-all duration-300 px-3 py-2"
-              type="email"
-              maxLength="100"
+              className='bg-[#10172d] w-full border rounded-md border-[#353a52] focus:border-[#16f2b3] ring-0 outline-0 transition-all duration-300 px-3 py-2'
+              type='email'
+              maxLength='100'
               required={true}
               value={input.email}
               onChange={(e) => setInput({ ...input, email: e.target.value })}
@@ -109,21 +114,23 @@ function ContactWithCaptcha() {
                 setError({ ...error, email: !isValidEmail(input.email) });
               }}
             />
-            {error.email &&
-              <p className="text-sm text-red-400">Please provide a valid email!</p>
-            }
+            {error.email && (
+              <p className='text-sm text-red-400'>
+                Please provide a valid email!
+              </p>
+            )}
           </div>
 
-          <div className="flex flex-col gap-2">
-            <label className="text-base">Your Message: </label>
+          <div className='flex flex-col gap-2'>
+            <label className='text-base'>Your Message: </label>
             <textarea
-              className="bg-[#10172d] w-full border rounded-md border-[#353a52] focus:border-[#16f2b3] ring-0 outline-0 transition-all duration-300 px-3 py-2"
-              maxLength="500"
-              name="message"
+              className='bg-[#10172d] w-full border rounded-md border-[#353a52] focus:border-[#16f2b3] ring-0 outline-0 transition-all duration-300 px-3 py-2'
+              maxLength='500'
+              name='message'
               required={true}
               onChange={(e) => setInput({ ...input, message: e.target.value })}
               onBlur={checkRequired}
-              rows="4"
+              rows='4'
               value={input.message}
             />
           </div>
@@ -131,25 +138,24 @@ function ContactWithCaptcha() {
             sitekey={process.env.NEXT_PUBLIC_RECAPTCHA_SITE_KEY}
             onChange={(code) => setCaptcha(code)}
           />
-          <div className="flex flex-col items-center gap-2">
-            {error.required &&
-              <p className="text-sm text-red-400">
+          <div className='flex flex-col items-center gap-2'>
+            {error.required && (
+              <p className='text-sm text-red-400'>
                 Email and Message are required!
               </p>
-            }
+            )}
             <button
-              className="flex items-center gap-1 hover:gap-3 rounded-full bg-gradient-to-r from-pink-500 to-violet-600 px-5 md:px-12 py-2.5 md:py-3 text-center text-xs md:text-sm font-medium uppercase tracking-wider text-white no-underline transition-all duration-200 ease-out hover:text-white hover:no-underline md:font-semibold"
-              role="button"
-              onClick={handleSendMail}
-            >
-              <span>Send Message</span>
-              <TbMailForward className="mt-1" size={18} />
+              className='flex items-center gap-1 hover:gap-3 rounded-full bg-gradient-to-r from-pink-500 to-violet-600 px-5 md:px-12 py-2.5 md:py-3 text-center text-xs md:text-sm font-medium uppercase tracking-wider text-white no-underline transition-all duration-200 ease-out hover:text-white hover:no-underline md:font-semibold'
+              role='button'
+              onClick={handleSendMail}>
+              <span>Send</span>
+              <TbMailForward className='mt-1' size={18} />
             </button>
           </div>
         </div>
       </div>
     </div>
   );
-};
+}
 
 export default ContactWithCaptcha;
--- next.config.js
+++ next.config.js
@@ -1,10 +1,23 @@
 const path = require('path')
- 
+
 module.exports = {
   sassOptions: {
     includePaths: [path.join(__dirname, 'styles')],
   },
   images: {
-    domains: ['res.cloudinary.com', 'media.dev.to']
-  }
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'res.cloudinary.com',
+        port: '',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'media.dev.to',
+        port: '',
+        pathname: '/**',
+      },
+    ],
+  },
 }

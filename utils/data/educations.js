0d2da0c2f7f50aba74626a46f62a44bcc5c2c460
Nanahawaw <folarinyetunde@gmail.com>
2024-05-16 00:44:50 +0100

    removed education section

--- utils/data/educations.js
+++ utils/data/educations.js
@@ -5,16 +5,5 @@ export const educations = [
     duration: "2017 - 2021",
     institution: "Federal University of Agriculture Abeokuta Nigeria",
   },
-  {
-    id: 2,
-    title: "Higher Secondary Certificate",
-    duration: "2018 - 2020",
-    institution: "Noakhali Islamia Kamil Madrasah",
-  },
-  {
-    id: 3,
-    title: "Secondary School Certificate",
-    duration: "2008 - 2018",
-    institution: "Baitus Saif Islamia Madrasah",
-  }
+
 ]

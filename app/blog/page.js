d6afc7bd87203543857049bab5611e1639451c4e
Nanahawaw <folarinyetunde@gmail.com>
2024-05-16 01:38:30 +0100

    renamed blog page to product page

--- app/blog/page.js
+++ app/blog/page.js
@@ -23,7 +23,7 @@ async function page() {
         <div className="flex  items-center">
           <span className="w-24 h-[2px] bg-[#1a1443]"></span>
           <span className="bg-[#1a1443] w-fit text-white p-2 px-5 text-2xl rounded-md">
-            All Blog
+            All Projects
           </span>
           <span className="w-24 h-[2px] bg-[#1a1443]"></span>
         </div>

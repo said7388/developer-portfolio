9d862b7bb566de24ec53f12659bd9200fa2a64d4
Nanahawaw <folarinyetunde@gmail.com>
2024-05-17 19:56:37 +0100

    added view more button

--- app/components/homepage/all-projects/index.jsx
+++ app/components/homepage/all-projects/index.jsx
@@ -1,6 +1,8 @@
 'use client';
 import React, { useEffect, useState } from 'react';
 import ProjectCard from './ProjectCard.jsx';
+import { FaArrowRight } from 'react-icons/fa';
+import Link from 'next/link';
 
 const AllProjects = () => {
   const [projects, setProjects] = useState([]);
@@ -37,6 +39,15 @@ const AllProjects = () => {
         {projects.map((project, i) => (
           <ProjectCard key={i} project={project} />
         ))}
+      </div>
+      <div className='flex justify-center  mt-5 lg:mt-12'>
+        <Link
+          className='flex items-center gap-1 hover:gap-3 rounded-full bg-gradient-to-r from-pink-500 to-violet-600 px-3 md:px-8 py-3 md:py-4 text-center text-xs md:text-sm font-medium uppercase tracking-wider text-white no-underline transition-all duration-200 ease-out hover:text-white hover:no-underline md:font-semibold'
+          role='button'
+          href='/projecs'>
+          <span>View More</span>
+          <FaArrowRight size={16} />
+        </Link>
       </div>
     </div>
   );

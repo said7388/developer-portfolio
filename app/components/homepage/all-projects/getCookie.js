d6af33c6e361256df1a4e4f4a8fd8e720c5e5719
Nanahawaw <folarinyetunde@gmail.com>
2024-05-18 17:18:52 +0100

    added function to get token from cookie

---
+++ app/components/homepage/all-projects/getCookie.js
@@ -0,0 +1,6 @@
+
+export function getCookie(name) {
+    const value = `; ${document.cookie}`;
+    const parts = value.split(`; ${name}=`);
+    if (parts.length === 2) return parts.pop().split(';').shift();
+}

2fe43a3f0b8ec8d81890a18641fdba612f33a350
Amitsha <135249871+Amit-Shatagar@users.noreply.github.com>
2024-07-12 09:05:42 +0530

    Update experience.js

--- utils/data/experience.js
+++ utils/data/experience.js
@@ -1,20 +1,15 @@
 export const experiences = [
   {
-    id: 1,
-    title: 'Software Engineer I',
-    company: "Teton Private Ltd.",
-    duration: "(Jan 2022 - Present)"
-  },
-  {
-    id: 2,
-    title: "FullStack Developer",
-    company: "Fiverr (freelance)",
-    duration: "(Jun 2021 - Jan 2022)"
-  },
-  {
-    id: 3,
-    title: "Self Employed",
-    company: "Code and build something in everyday.",
-    duration: "(Jan 2018 - Present)"
-  }
+        id: 1,
+        title: 'FullStack developer',
+        type: 'Intership',
+        company: "AICTE",
+        duration: "(Feb 2024 - Apr 2024)"
+      },
+      {
+        id: 2,
+        title: "Data Analytics with AI & Cloud",
+        company: "AICTE",
+        duration: "(Feb 2024 - May 2024)"
+      }
 ]

2fee5d090ce1fe37fd2fd5fbecf699e7fc6a20b9
Amitsha <135249871+Amit-Shatagar@users.noreply.github.com>
2024-07-16 06:20:14 +0000

    update

--- app/layout.js
+++ app/layout.js
@@ -10,9 +10,9 @@ import ScrollToTop from "./components/helper/scroll-to-top";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
-  title: "Portfolio of Amit Shat - Fresher Portfolio",
+  title: "👨‍💻📈 Amit Shataga - Portfolio 🎯✨ ",
   description:
-    "This is the portfolio of Abu Said. I am a full stack developer and a self taught developer. I love to learn new things and I am always open to collaborating with others. I am a quick learner and I am always looking for new challenges.",
+    "This is the portfolio of Amit Shatagar. I am a Fresher full stack developer and a self taught developer. I love to learn new things and I am always open to collaborating with others. I am a quick learner and I am always looking for new challenges.",
 };
 
 export default function RootLayout({ children }) {
--- utils/data/experience.js
+++ utils/data/experience.js
@@ -3,14 +3,14 @@ export const experiences = [
         id: 1,
         title: 'FullStack developer',
         company: "AICTE",
-        type: 'Type - Internship',
+        type: 'TYPE - INTERNSHIP',
         duration: "(Feb 2024 - Apr 2024)"
       },
       {
         id: 2,
         title: "Data Analytics with AI & Cloud",
         company: "AICTE",
-        type: 'Type - Internship',
+        type: 'TYPE - INTERNSHIP',
         duration: "(Feb 2024 - May 2024)"
       }
 ]

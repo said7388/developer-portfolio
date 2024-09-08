b3aed4a0ae0beb421ce5fe50a1fe95fad03e9381
Rajeev Singh <rsinghh68@gmail.com>
2024-09-08 10:23:41 +0530

    Remove hard coded names and designations

--- app/components/navbar.jsx
+++ app/components/navbar.jsx
@@ -1,5 +1,6 @@
 // @flow strict
 import Link from "next/link";
+import { personalData } from "@/utils/data/personal-data";
 
 
 function Navbar() {
@@ -10,7 +11,7 @@ function Navbar() {
           <Link
             href="/"
             className=" text-[#16f2b3] text-3xl font-bold">
-            ABU SAID
+            {personalData.name}
           </Link>
         </div>
 

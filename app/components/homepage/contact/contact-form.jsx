d82173b14875e899fd634af75d828a1826cacf64
said7388 <mdabusaidriyaz@gmail.com>
2024-10-20 00:21:06 +0600

    update contact form

--- app/components/homepage/contact/contact-form.jsx
+++ app/components/homepage/contact/contact-form.jsx
@@ -5,10 +5,10 @@ import axios from "axios";
 import { useState } from "react";
 import { TbMailForward } from "react-icons/tb";
 import { toast } from "react-toastify";
-import emailjs from "@emailjs/browser";
 
-function ContactWithoutCaptcha() {
+function ContactForm() {
   const [error, setError] = useState({ email: false, required: false });
+  const [isLoading, setIsLoading] = useState(false);
   const [userInput, setUserInput] = useState({
     name: "",
     email: "",
@@ -23,6 +23,7 @@ function ContactWithoutCaptcha() {
 
   const handleSendMail = async (e) => {
     e.preventDefault();
+
     if (!userInput.email || !userInput.message || !userInput.name) {
       setError({ ...error, required: true });
       return;
@@ -30,30 +31,30 @@ function ContactWithoutCaptcha() {
       return;
     } else {
       setError({ ...error, required: false });
-    }
-
-    const serviceID = process.env.NEXT_PUBLIC_EMAILJS_SERVICE_ID;
-    const templateID = process.env.NEXT_PUBLIC_EMAILJS_TEMPLATE_ID;
-    const options = { publicKey: process.env.NEXT_PUBLIC_EMAILJS_PUBLIC_KEY };
+    };
 
     try {
-      const res = await emailjs.send(serviceID, templateID, userInput, options);
-      const teleRes = await axios.post(`${process.env.NEXT_PUBLIC_APP_URL}/api/contact`, userInput);
-      if (res.status === 200 || teleRes.status === 200) {
-        toast.success("Message sent successfully!");
-        setUserInput({
-          name: "",
-          email: "",
-          message: "",
-        });
-      }
+      setIsLoading(true);
+      const res = await axios.post(
+        `${process.env.NEXT_PUBLIC_APP_URL}/api/contact`,
+        userInput
+      );
+
+      toast.success("Message sent successfully!");
+      setUserInput({
+        name: "",
+        email: "",
+        message: "",
+      });
     } catch (error) {
-      toast.error(error?.text || error);
-    }
+      toast.error(error?.response?.data?.message);
+    } finally {
+      setIsLoading(false);
+    };
   };
 
   return (
-    <div className="">
+    <div>
       <p className="font-medium mb-5 text-[#16f2b3] text-xl uppercase">Contact with me</p>
       <div className="max-w-3xl text-white rounded-lg border border-[#464c6a] p-3 lg:p-5">
         <p className="text-sm text-[#d3d8e8]">{"If you have any questions or concerns, please don't hesitate to contact me. I am open to any work opportunities that align with my skills and interests."}</p>
@@ -101,21 +102,30 @@ function ContactWithoutCaptcha() {
               value={userInput.message}
             />
           </div>
-          <div className="flex flex-col items-center gap-2">
-            {error.required && <p className="text-sm text-red-400">Email and Message are required!</p>}
+          <div className="flex flex-col items-center gap-3">
+            {error.required && <p className="text-sm text-red-400">
+              All fiels are required!
+            </p>}
             <button
               className="flex items-center gap-1 hover:gap-3 rounded-full bg-gradient-to-r from-pink-500 to-violet-600 px-5 md:px-12 py-2.5 md:py-3 text-center text-xs md:text-sm font-medium uppercase tracking-wider text-white no-underline transition-all duration-200 ease-out hover:text-white hover:no-underline md:font-semibold"
               role="button"
               onClick={handleSendMail}
+              disabled={isLoading}
             >
-              <span>Send Message</span>
-              <TbMailForward className="mt-1" size={18} />
+              {
+                isLoading ?
+                <span>Sending Message...</span>:
+                <span className="flex items-center gap-1">
+                  Send Message
+                  <TbMailForward size={20} />
+                </span>
+              }
             </button>
           </div>
         </div>
       </div>
     </div>
   );
-}
+};
 
-export default ContactWithoutCaptcha;
+export default ContactForm;
--- app/components/homepage/contact/index.jsx
+++ app/components/homepage/contact/index.jsx
@@ -7,8 +7,7 @@ import { FaFacebook, FaStackOverflow } from 'react-icons/fa';
 import { FaXTwitter } from "react-icons/fa6";
 import { IoLogoGithub, IoMdCall } from "react-icons/io";
 import { MdAlternateEmail } from "react-icons/md";
-import ContactWithCaptcha from './contact-with-captcha';
-import ContactWithoutCaptcha from './contact-without-captcha';
+import ContactForm from './contact-form';
 
 function ContactSection() {
   return (
@@ -20,11 +19,7 @@ function ContactSection() {
         <span className="h-36 w-[2px] bg-[#1a1443]"></span>
       </div>
       <div className="grid grid-cols-1 lg:grid-cols-2 gap-8 lg:gap-16 items-center">
-        {
-          (process.env.NEXT_PUBLIC_RECAPTCHA_SITE_KEY && process.env.NEXT_PUBLIC_RECAPTCHA_SECRET_KEY) ? <ContactWithCaptcha />
-            : <ContactWithoutCaptcha />
-        }
-
+        <ContactForm />
         <div className="lg:w-3/4 ">
           <div className="flex flex-col gap-5 lg:gap-9">
             <p className="text-sm md:text-xl flex items-center gap-3">

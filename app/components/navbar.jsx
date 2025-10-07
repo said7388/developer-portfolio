2604f8144e6dacbfd2140e5a3efe77261ba3d8b3
Joshua McNabb <joshuamcnabb1995@gmail.com>
2025-10-07 16:31:11 -0600

    Refactor Home and Navbar components: comment out data fetching logic in Home and adjust Navbar for smoother scrolling to projects section.

--- app/components/navbar.jsx
+++ app/components/navbar.jsx
@@ -60,6 +60,16 @@ function Navbar() {
           <li>
             <a
               className="block px-4 py-2 no-underline outline-none hover:no-underline cursor-pointer"
+              onClick={(e) => handleSmoothScroll(e, "projects")}
+            >
+              <div className="text-sm text-white transition-colors duration-300 hover:text-pink-600">
+                PROJECTS
+              </div>
+            </a>
+          </li>
+          <li>
+            <a
+              className="block px-4 py-2 no-underline outline-none hover:no-underline cursor-pointer"
               onClick={(e) => handleSmoothScroll(e, "education")}
             >
               <div className="text-sm text-white transition-colors duration-300 hover:text-pink-600">
@@ -67,7 +77,7 @@ function Navbar() {
               </div>
             </a>
           </li>
-          <li>
+          {/* <li>
             <Link
               className="block px-4 py-2 no-underline outline-none hover:no-underline"
               href="/blog"
@@ -76,17 +86,7 @@ function Navbar() {
                 BLOGS
               </div>
             </Link>
-          </li>
-          <li>
-            <a
-              className="block px-4 py-2 no-underline outline-none hover:no-underline cursor-pointer"
-              onClick={(e) => handleSmoothScroll(e, "projects")}
-            >
-              <div className="text-sm text-white transition-colors duration-300 hover:text-pink-600">
-                PROJECTS
-              </div>
-            </a>
-          </li>
+          </li> */}
         </ul>
       </div>
     </nav>
--- app/page.js
+++ app/page.js
@@ -8,33 +8,37 @@ import HeroSection from "./components/homepage/hero-section";
 import Projects from "./components/homepage/projects";
 import Skills from "./components/homepage/skills";
 
-async function getData() {
-  const res = await fetch(`https://dev.to/api/articles?username=${personalData.devUsername}`)
+// async function getData() {
+//   const res = await fetch(
+//     `https://dev.to/api/articles?username=${personalData.devUsername}`
+//   );
 
-  if (!res.ok) {
-    throw new Error('Failed to fetch data')
-  }
+//   if (!res.ok) {
+//     throw new Error("Failed to fetch data");
+//   }
 
-  const data = await res.json();
+//   const data = await res.json();
 
-  const filtered = data.filter((item) => item?.cover_image).sort(() => Math.random() - 0.5);
+//   const filtered = data
+//     .filter((item) => item?.cover_image)
+//     .sort(() => Math.random() - 0.5);
 
-  return filtered;
-};
+//   return filtered;
+// }
 
 export default async function Home() {
-  const blogs = await getData();
+  // const blogs = await getData();
 
   return (
-    <div suppressHydrationWarning >
+    <div suppressHydrationWarning>
       <HeroSection />
       <AboutSection />
       <Experience />
       <Skills />
       <Projects />
       <Education />
-      <Blog blogs={blogs} />
+      {/* <Blog blogs={blogs} /> */}
       <ContactSection />
     </div>
-  )
-};
+  );
+}

7699b97db59ab6c982e8ef91c45d69805e6d027f
Joshua McNabb <joshuamcnabb1995@gmail.com>
2025-10-08 11:35:19 -0600

    Update ProjectCard component: replace single quotes with HTML entities for role and description to ensure proper rendering in JSX.

--- app/components/homepage/projects/project-card.jsx
+++ app/components/homepage/projects/project-card.jsx
@@ -51,12 +51,14 @@ function ProjectCard({ project }) {
           </div>
           <div>
             <span className="ml-4 lg:ml-8 mr-2 text-white">myRole:</span>
-            <span className="text-orange-400">'{project.role}'</span>
+            <span className="text-orange-400">&apos;{project.role}&apos;</span>
             <span className="text-gray-400">,</span>
           </div>
           <div className="ml-4 lg:ml-8 mr-2">
             <span className="text-white">description: </span>
-            <span className="text-cyan-400">'{project.description}'</span>
+            <span className="text-cyan-400">
+              &apos;{project.description}&apos;
+            </span>
             <span className="text-gray-400">,</span>
           </div>
           <div>

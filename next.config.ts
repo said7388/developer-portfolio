cc26cc316ddbcdebc8805960d4efd2626ff8393a
Joshua McNabb <joshuamcnabb1995@gmail.com>
2025-10-08 10:25:34 -0600

    Replace next.config.js with next.config.ts for TypeScript support and maintain existing configuration settings.

---
+++ next.config.ts
@@ -0,0 +1,26 @@
+import path from "path";
+
+module.exports = {
+  sassOptions: {
+    includePaths: [path.join(__dirname, "styles")],
+  },
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "res.cloudinary.com",
+        pathname: "**",
+      },
+      {
+        protocol: "https",
+        hostname: "media.dev.to",
+        pathname: "**",
+      },
+      {
+        protocol: "https",
+        hostname: "media2.dev.to",
+        pathname: "**",
+      },
+    ],
+  },
+};

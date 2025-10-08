8955fe4f5a2b0b87c2b2bee7b8fe50d806d2528f
Joshua McNabb <joshuamcnabb1995@gmail.com>
2025-10-08 17:29:12 -0600

    Update AboutSection and personal data: Revise heading text for clarity and enhance personal description with a more engaging narrative.

--- app/components/homepage/about/index.jsx
+++ app/components/homepage/about/index.jsx
@@ -15,13 +15,13 @@ function AboutSection() {
       <div className="grid grid-cols-1 lg:grid-cols-2 gap-8 lg:gap-16">
         <div className="order-2 lg:order-1">
           <p className="font-medium mb-5 text-[#16f2b3] text-xl uppercase">
-            Who I am?
+            Who am I?
           </p>
-          <p className="text-gray-200 text-sm lg:text-lg">
+          <p className="text-gray-200 text-sm lg:text-lg whitespace-pre-line">
             {personalData.description}
           </p>
         </div>
-        <div className="flex justify-center order-1 lg:order-2">
+        <div className="flex justify-center items-start order-1 lg:order-2">
           <Image
             src={personalData.profile}
             width={280}

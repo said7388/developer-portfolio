dbc0248a56bcb54ec7dfb1eb9be00cda28e3cac9
Joshua McNabb <joshuamcnabb1995@gmail.com>
2025-10-08 09:58:57 -0600

    Update favicon and modify portfolio title for clarity

--- app/layout.js
+++ app/layout.js
@@ -10,7 +10,7 @@ import "./css/globals.scss";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
-  title: "Portfolio of Joshua McNabb - Software Developer",
+  title: "Joshua McNabb - Software Developer",
   description:
     "This is the portfolio of Joshua McNabb. I am a full stack developer and a self taught developer. I love to learn new things and I am always open to collaborating with others. I am a quick learner and I am always looking for new challenges.",
 };

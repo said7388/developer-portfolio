b311374e7a2871d4fb2361aee5c7539cddc4e25f
charbelB7 <135625668+charbelB7@users.noreply.github.com>
2025-11-21 08:36:29 -0500

    Update contactsData.js

--- utils/data/contactsData.js
+++ utils/data/contactsData.js
@@ -1,11 +1,8 @@
 export const contactsData = {
-    email: 'abusaid7388@gmail.com',
-    phone: '+8801608797655',
-    address: 'Middle Badda, Dhaka, Bangladesh - 1212 ',
-    github: 'https://github.com/said7388',
-    facebook: 'https://www.facebook.com/abusaid.riyaz/',
-    linkedIn: 'https://www.linkedin.com/in/abu-said-bd/',
-    twitter: 'https://twitter.com/said7388',
-    stackOverflow: 'https://stackoverflow.com/users/16840768/abu-said',
-    devUsername: "said7388"
+    email: 'charbel.barakat.ing@gmail.com',
+    phone: '+15149618383',
+    address: 'Montréal, QC, Canada',
+    github: 'https://github.com/charbelB7',
+    linkedIn: 'https://www.linkedin.com/in/charbelB7/'
+  
 }

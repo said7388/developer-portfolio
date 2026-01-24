20f28a492ff9b2b2e21ae6b39d9d493918828254
Mostafa Alazhariy <87443208+MAlazhariy@users.noreply.github.com>
2026-01-25 01:36:39 +0200

    Import personal data in footer component for enhanced functionality

--- app/components/footer.jsx
+++ app/components/footer.jsx
@@ -2,6 +2,7 @@
 import Link from 'next/link';
 import { CgGitFork } from "react-icons/cg";
 import { IoStar } from "react-icons/io5";
+import { personalData } from '../../utils/data/personal-data';
 
 function Footer() {
   return (

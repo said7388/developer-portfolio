27a46547db9f3f18a8047a868cda7d16757e406e
Mostafa Alazhariy <87443208+MAlazhariy@users.noreply.github.com>
2026-01-24 17:28:03 +0200

    Update personal and contact information; add WhatsApp and YouTube links to contact section, and remove Twitter and LeetCode links from hero section.

--- app/components/homepage/contact/index.jsx
+++ app/components/homepage/contact/index.jsx
@@ -3,8 +3,7 @@ import { personalData } from '@/utils/data/personal-data';
 import Link from 'next/link';
 import { BiLogoLinkedin } from "react-icons/bi";
 import { CiLocationOn } from "react-icons/ci";
-import { FaFacebook, FaStackOverflow } from 'react-icons/fa';
-import { FaXTwitter } from "react-icons/fa6";
+import { FaFacebook, FaStackOverflow, FaWhatsapp, FaYoutube } from 'react-icons/fa';
 import { IoLogoGithub, IoMdCall } from "react-icons/io";
 import { MdAlternateEmail } from "react-icons/md";
 import ContactForm from './contact-form';
@@ -61,8 +60,14 @@ function ContactSection() {
                 size={48}
               />
             </Link>
-            <Link target="_blank" href={personalData.twitter}>
-              <FaXTwitter
+            <Link target="_blank" href={personalData.whatsApp}>
+              <FaWhatsapp
+                className="bg-[#8b98a5] p-3 rounded-full hover:bg-[#16f2b3] hover:scale-110 transition-all duration-300 text-gray-800 cursor-pointer"
+                size={48}
+              />
+            </Link>
+            <Link target="_blank" href={personalData.youtube}>
+              <FaYoutube
                 className="bg-[#8b98a5] p-3 rounded-full hover:bg-[#16f2b3] hover:scale-110 transition-all duration-300 text-gray-800 cursor-pointer"
                 size={48}
               />
--- utils/data/contactsData.js
+++ utils/data/contactsData.js
@@ -1,11 +1,13 @@
 export const contactsData = {
-    email: 'abusaid7388@gmail.com',
-    phone: '+8801608797655',
-    address: 'Middle Badda, Dhaka, Bangladesh - 1212 ',
-    github: 'https://github.com/said7388',
-    facebook: 'https://www.facebook.com/abusaid.riyaz/',
-    linkedIn: 'https://www.linkedin.com/in/abu-said-bd/',
-    twitter: 'https://twitter.com/said7388',
-    stackOverflow: 'https://stackoverflow.com/users/16840768/abu-said',
-    devUsername: "said7388"
+    email: 'mostafa.alazhariy@gmail.com',
+    phone: '+201013688985',
+    address: 'Cairo, Egypt',
+    github: 'https://github.com/malazhariy',
+    facebook: 'https://www.facebook.com/malazhariy/',
+    linkedIn: 'https://www.linkedin.com/in/malazhariy/',
+    // twitter: 'https://twitter.com/malazhariy',
+    stackOverflow: 'https://stackoverflow.com/users/16449728/mostafa-alazhariy',
+    whatsApp: 'https://api.whatsapp.com/send/?phone=201013688985',
+    youtube: 'https://www.youtube.com/@MAlazhariy',
+    devUsername: "malazhariy"
 }
